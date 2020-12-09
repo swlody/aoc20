11953b923ece038c7a0d6283ca7ff1e77d2fd17d
Sam Wlody <swlody@gmail.com>
2020-12-09 01:53:52 -0500

    Some day 9 cleanup, kind of

--- src/day09.rs
+++ src/day09.rs
@@ -2,8 +2,7 @@ pub fn input_generator(input: &str) -> Vec<u64> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-fn solve_part1_with_preamble_length(preamble_length: usize, numbers: &[u64]) -> u64 {
-    let mut found = 0;
+fn solve_part1_with_preamble_length(numbers: &[u64], preamble_length: usize) -> u64 {
     'outer: for (idx, &number) in numbers.iter().enumerate().skip(preamble_length) {
         let window = &numbers[idx - preamble_length..idx];
         for &other_number in window.iter() {
@@ -15,11 +14,9 @@ fn solve_part1_with_preamble_length(preamble_length: usize, numbers: &[u64]) -> 
                 continue 'outer;
             }
         }
-
-        found = number;
-        break;
+        return number;
     }
-    found
+    panic!("No answer found")
 }
 
 use std::cmp::Ordering;
@@ -27,18 +24,14 @@ use std::cmp::Ordering;
 fn solve_part2_from_part1(numbers: &[u64], part1_result: u64) -> u64 {
     'outer: for start_point in 0..numbers.len() {
         let mut result = 0;
-        let mut offset = 0;
-        loop {
+        let (mut min, mut max) = (u64::MAX, 0);
+        for offset in 0..numbers.len() - start_point {
             let next_number = numbers[start_point + offset];
             match (result + next_number).cmp(&part1_result) {
-                Ordering::Equal if offset > 0 => {
-                    let range = &numbers[start_point..start_point + offset];
-                    let low_value = range.iter().min().unwrap();
-                    let high_value = range.iter().max().unwrap();
-                    return low_value + high_value;
-                }
+                Ordering::Equal if offset > 0 => return min + max,
                 Ordering::Less => {
-                    offset += 1;
+                    min = std::cmp::min(min, next_number);
+                    max = std::cmp::max(max, next_number);
                     result += next_number;
                 }
                 _ => continue 'outer,
@@ -48,9 +41,9 @@ fn solve_part2_from_part1(numbers: &[u64], part1_result: u64) -> u64 {
     panic!("No answer found")
 }
 
-pub struct Answer(u64, u64);
-
 use std::fmt;
+
+pub struct Answer(u64, u64);
 impl fmt::Display for Answer {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Part 1: {}, Part 2: {}", self.0, self.1)
@@ -58,7 +51,7 @@ impl fmt::Display for Answer {
 }
 
 pub fn solve_both_parts(input: &[u64]) -> Answer {
-    let part1_result = solve_part1_with_preamble_length(25, input);
+    let part1_result = solve_part1_with_preamble_length(input, 25);
     let part2_result = solve_part2_from_part1(input, part1_result);
     Answer(part1_result, part2_result)
 }
@@ -91,7 +84,7 @@ mod tests {
     #[test]
     fn test_both_parts() {
         let input = input_generator(INPUT);
-        let part1_result = solve_part1_with_preamble_length(5, &input);
+        let part1_result = solve_part1_with_preamble_length(&input, 5);
         assert_eq!(127, part1_result);
         let part2_result = solve_part2_from_part1(&input, part1_result);
         assert_eq!(62, part2_result);
